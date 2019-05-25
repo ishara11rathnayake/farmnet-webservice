f5fb5c276249e0c2fb7aa08c926572028a3575a8
ishara11rathnayake <ishara11rathnayake@gmail.com>
2019-05-26 01:05:16 +0530

    adding controllers

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -3,7 +3,6 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ");
-    console.log(token[1]);
     const decoded = jwt.verify(token[1], process.env.JWT_KEY);
     req.userData = decoded;
     next();
